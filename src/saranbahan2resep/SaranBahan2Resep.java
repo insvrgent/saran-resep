918cedf138042dc5cbe57d02ceee92a804322c7f
user <user@z>
2020-11-28 14:53:21 +0700

    TambahTampilResep

--- src/saranbahan2resep/SaranBahan2Resep.java
+++ src/saranbahan2resep/SaranBahan2Resep.java
@@ -34,6 +34,40 @@ public class SaranBahan2Resep {
             }
             System.out.print("Masukkan no pilihan Anda");
             int bahan2 = scanner.nextInt();
+            
+            if (bahan2 >= 1 && bahan2 <= 3)
+            {
+                if(bahan1==1)
+                {
+                    switch(bahan2)
+                    {
+                        case 1: System.out.println(
+                                "Anda dapat membuat milk shake banana"); break;
+                        case 2: System.out.println(
+                                "Anda dapat membuat pisang goreng"); break;
+                        case 3: System.out.println(
+                                "Anda dapat membuat pisang rebus"); break;
+                        
+                    }
+                }
+                else
+                {
+                    switch(bahan2)
+                    {
+                        case 1: System.out.println(
+                                "Anda dapat membuat telur mata sapi"); break;
+                        case 2: System.out.println(
+                                "Anda dapat membuat sandwich telur"); break;
+                        case 3: System.out.println(
+                                "Anda dapat membuat telur rebus"); break;
+                    }
+                }
+            }
+            else
+            {
+                System.out.println("Mohon Maaf, Pilihan tidak ditemukan, "
+                    + "tidak dapat memberikan saran resep");
+            }
         }
         else
             System.out.println("Mohon Maaf, Pilihan tidak ditemukan, "
